58e96774f82927fb18472917fddd9572b678d631
SeharShakir <sahar_shakir@hotmail.com>
2026-08-03 16:03:01 -0400

    assignment changes

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -54,6 +54,7 @@ rm -f data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
+find data/processed -type f > ./data/inventory.txt
 
 ###########################################
 
